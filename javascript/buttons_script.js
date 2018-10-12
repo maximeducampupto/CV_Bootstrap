69ffa533f2ed27b22aafc2e6806593827a696174
maximeducampupto <43751403+maximeducampupto@users.noreply.github.com>
2018-10-12 16:14:14 +0200

    Added Contact Form

--- javascript/buttons_script.js
+++ javascript/buttons_script.js
@@ -5,10 +5,12 @@ document.addEventListener('click', function(e) {
         switch(wrapper.style.display) {
             case 'none':
             wrapper.style.display = 'flex';
+                wrapper.parentElement.style.marginBottom = '70px';
             e.target.innerHTML = '-';
             break;
             default:
                 wrapper.style.display = 'none';
+                wrapper.parentElement.style.marginBottom = '20px';
                 e.target.innerHTML = '+';
         }
     }
