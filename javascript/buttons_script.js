2b9840a8b4b65704785baab2a02bc7127a9ce0bc
maximeducampupto <43751403+maximeducampupto@users.noreply.github.com>
2018-10-12 11:22:03 +0200

    Initial commit

---
+++ javascript/buttons_script.js
@@ -0,0 +1,32 @@
+document.getElementById('exp_button').addEventListener('click', function() {
+   var wrapper = document.getElementById('exp_wrapper');
+
+   if (wrapper.style.display != 'none') {
+       wrapper.style.display = 'none';
+       document.getElementById('exp_button').innerHTML = '+';
+   } else {
+       wrapper.style.display = 'flex';
+   }
+});
+
+document.getElementById('form_button').addEventListener('click', function() {
+    var wrapper = document.getElementById('form_wrapper');
+
+    if (wrapper.style.display != 'none') {
+        wrapper.style.display = 'none';
+        document.getElementById('form_button').innerHTML = '+';
+    } else {
+        wrapper.style.display = 'flex';
+    }
+});
+
+document.getElementById('comp_button').addEventListener('click', function() {
+    var wrapper = document.getElementById('comp_wrapper');
+
+    if (wrapper.style.display != 'none') {
+        wrapper.style.display = 'none';
+        document.getElementById('comp_wrapper').innerHTML = '+';
+    } else {
+        wrapper.style.display = 'flex';
+    }
+});
